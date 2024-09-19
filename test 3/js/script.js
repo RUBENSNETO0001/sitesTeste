1c21621c5af36c0480f8003e3446a4389fd6cc81
RUBENS NETO <121876888+RUBENSNETO0001@users.noreply.github.com>
2024-09-19 08:53:28 -0500

    vs2

--- test 3/js/script.js
+++ test 3/js/script.js
@@ -1,9 +1,32 @@
 alert("Esse programa vai soma dois valores e mostra para o usuario");
 alert("Caso esse usuario queira refazer a conta, atualize a pagina");
+
 function calcular(){
     $numero01 = parseInt(document.getElementById("n1").value);
     $numero02 = parseInt(document.getElementById("n2").value);
+    $formadecalcular = document.getElementById("formaDeCalculo").value;
 
-    $resultado = $numero01 + $numero02;
-    document.getElementById("resultado").innerText = $resultado;
+    if($formadecalcular == "+"){
+         $resultado = $numero01 + $numero02;
+         document.getElementById("resultado").innerText = $resultado;
+    }
+    else if($formadecalcular == "-"){
+        $resultado = $numero01 - $numero02;
+        document.getElementById("resultado").innerText = $resultado;
+    }
+    else if($formadecalcular == "x"){
+        $resultado = $numero01 * $numero02;
+        document.getElementById("resultado").innerText = $resultado;
+    }
+    else if($formadecalcular == "/"){
+        $resultado = $numero01 / $numero02;
+        document.getElementById("resultado").innerText = $resultado;
+    }
+    else if($formadecalcular == "%"){
+        $resultado = $numero01 % $numero02;
+        document.getElementById("resultado").innerText = $resultado;
+    }
+    else{
+        alert("A forma de calcular não foi colocada");
+    }
 }
